9dd58a6c82de261289b80c4bb0b84f90d212b91c
isaacp.official@gmail.com <58235609+iamisaacp@users.noreply.github.com>
2022-10-28 07:47:29 +0100

    corsh

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require("cors")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const cors = require('cors');
 
 var app = express();
 
